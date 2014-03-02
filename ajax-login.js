e6c135982f103f25633f5b4d8ed6cc4da5ec2f14
Brian Zalewski <brian@enviedsolutions.com>
2014-03-02 04:19:13 -0500

    Added ajax-login.js and modified app.less

---
+++ ajax-login.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+    $('form#login').on('submit', function(e){
+        $('form#login p.status').show().text(ajax_login_object.loadingmessage);
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: ajax_login_object.ajaxurl,
+            data: {
+                'action': 'ajaxlogin', //calls wp_ajax_nopriv_ajaxlogin
+                'username': $('form#login #username').val(),
+                'password': $('form#login #password').val(),
+                'security': $('form#login #security').val() },
+            success: function(data){
+                $('form#login p.status').text(data.message);
+                if (data.loggedin == true){
+                    document.location.href = ajax_login_object.redirecturl;
+                }
+            }
+        });
+        e.preventDefault();
+    });
+
+});
