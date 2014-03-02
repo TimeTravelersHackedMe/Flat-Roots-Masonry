a343c9905a129018698c698db23cab4cb4e82ecc
Brian Zalewski <brian@enviedsolutions.com>
2014-03-02 04:12:23 -0500

    Updated Custom Functions and Header Top Navbar
    Added custom.php functions and custom header-top-navbar.php format.

--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,83 @@
 /**
  * Custom functions
  */
+function admin_bar_customize() {
+        global $wp_admin_bar;
+        $wp_admin_bar->remove_menu('wp-logo');
+        //$wp_admin_bar->remove_menu('wpseo-menu');
+        //$wp_admin_bar->remove_menu('wp-admin-wordpress-social-login');
+        //$wp_admin_bar->remove_menu('nginx-helper-purge-all');
+}
+add_action('wp_before_admin_bar_render', 'admin_bar_customize', 999);
+function adjust_the_wp_menu() {
+        //remove_submenu_page('options-general.php', 'options-permalink.php');
+        //remove_submenu_page('options-general.php', 'options-writing.php');
+        //remove_submenu_page('options-general.php', 'crayon_settings' );
+}
+add_action( 'admin_menu', 'adjust_the_wp_menu', 999 );
+function block_subscribers_from_admin_dashboard() {
+    if ( is_admin() && !current_user_can('edit_posts') &&
+       ! ( defined( 'DOING_AJAX' ) && DOING_AJAX ) ) {
+        wp_redirect( home_url() );
+        exit;
+    }
+}
+add_action( 'admin_init', 'block_subscribers_from_admin_dashboard' );
+function disable_admin_bar_for_subscribers() {
+        if( ! current_user_can('edit_posts') )
+                add_filter('show_admin_bar', '__return_false');
+}
+add_action( 'after_setup_theme', 'disable_admin_bar_for_subscribers' );
+function roots_theme_customizer( $wp_customize ) {
+$wp_customize->add_section( 'roots_logo_section' , array(
+    'title'       => __( 'Logo', 'roots' ),
+    'priority'    => 30,
+    'description' => 'Upload a logo to replace the default site name and description in the header',
+) );
+$wp_customize->add_setting( 'roots_logo' );
+$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'roots_logo', array(
+    'label'    => __( 'Logo', 'roots' ),
+    'section'  => 'roots_logo_section',
+    'settings' => 'roots_logo',
+) ) );
+}
+add_action('customize_register', 'roots_theme_customizer');
+function ajax_login_init(){
+    wp_register_script('ajax-login-script', get_template_directory_uri() . '/ajax-login.js', array('jquery') );
+    wp_enqueue_script('ajax-login-script');
+
+
+    wp_localize_script( 'ajax-login-script', 'ajax_login_object', array(
+        'ajaxurl' => admin_url( 'admin-ajax.php' ),
+        'redirecturl' => home_url(),
+        'loadingmessage' => __('Sending user info, please wait...')
+    ));
+
+    // Enable the user with no privileges to run ajax_login() in AJAX
+    add_action( 'wp_ajax_nopriv_ajaxlogin', 'ajax_login' );
+}
+
+// Execute the action only if the user isn't logged in
+if (!is_user_logged_in()) {
+    add_action('init', 'ajax_login_init');
+}
+function ajax_login(){
+
+    // First check the nonce, if it fails the function will break
+    check_ajax_referer( 'ajax-login-nonce', 'security' );
+
+    // Nonce is checked, get the POST data and sign user on
+    $info = array();
+    $info['user_login'] = $_POST['username'];
+    $info['user_password'] = $_POST['password'];
+    $info['remember'] = true;
+
+    $user_signon = wp_signon( $info, false );
+    if ( is_wp_error($user_signon) ){
+        echo json_encode(array('loggedin'=>false, 'message'=>__('Wrong username or password.')));
+    } else {
+        echo json_encode(array('loggedin'=>true, 'message'=>__('Login successful, redirecting...')));
+    }
+
+    die();
+}
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -1,4 +1,6 @@
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
+<header class="banner navbar navbar-default navbar-fixed-top" role="banner"
+<?php if ( is_admin_bar_showing() ) echo ' style="margin-top:32px"';
+?>>
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -7,15 +9,68 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
-    </div>
-
+<?php if ( get_theme_mod( 'roots_logo' ) ) : ?>
+<a class="navbar-brand logo-brand" href="<?php echo home_url(); ?>/" title="<?php bloginfo('name'); ?>" rel="home"><img class="logo" src='<?php echo esc_url( get_theme_mod( 'roots_logo' ) ); ?>' alt="<?php bloginfo('name'); ?>"></a>
+<?php else : ?>
+<a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+<?php endif; ?>
+</div>
     <nav class="collapse navbar-collapse" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
         endif;
       ?>
-    </nav>
-  </div>
+<?php if (is_user_logged_in()) {  ?>
+    <a type="button" class="btn btn-primary navbar-btn navbar-right" href="<?php echo wp_logout_url( home_url() ); ?>"><em class="icon-signout icon-large"></em> Log Out</a>
+<?php } else { ?>
+<button type="button" data-toggle="modal" href="#signin" class="btn btn-primary navbar-btn navbar-right"><em class="icon-signin icon-large"></em> Log In</button>
+<?php } ?>
+</nav>
+</div>
 </header>
+<div class="modal fade" id="signin" tabindex="-1" role="dialog" aria-labelledby="LoginLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+          <h4 class="modal-title"><em class="icon-signin icon-large"></em> Log In</h4>
+        </div>
+        <div class="modal-body">
+        <p class="status"></p>
+<ul class="list-inline text-center">
+<li>
+<a href="" class="btn btn-icon btn-facebook"><em class="icon-facebook"></em> Facebook</a>
+</li>
+<li>
+<a href=""  class="btn btn-icon btn-twitter"><em class="icon-twitter"></em> Twitter</a>
+</li>
+<li>
+<a href="" class="btn btn-icon btn-google"><em class="icon-google-plus"></em> Google</a>
+</li>
+</ul>
+        <form id="login" action="login" method="post" role="form" >
+                    <div class="form-group">
+                      <div class="input-group input-icon">
+                        <span class="input-group-addon">
+                          <em class="icon-user icon-fixed-width"></em>
+                        </span>
+                        <input type="text" id="username" name="username" class="form-control" placeholder="Username">
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="input-group input-icon">
+                        <span class="input-group-addon">
+                          <em class="icon-key icon-fixed-width"></em>
+                        </span>
+                        <input type="password" id="password" name="password" class="form-control" placeholder="Password">
+                      </div>
+                    </div>
+                   <div class="form-group"><input type="submit" value="Log In" class="btn btn-primary pull-right">
+<?php wp_nonce_field( 'ajax-login-nonce', 'security' ); ?>
+</form>
+       <ul class="list-inline"><li><a href="<?php echo wp_lostpassword_url(); ?>">Lost your password?</a></li><li><a href="">Need to sign up?</a></li></ul></div>
+         </div>
+      </div>
+    </div>
+</div>
